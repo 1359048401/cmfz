640678c923373deb551160ef7bdc7b7b1fd90b2c
spj <1359048401@qq.com>
2018-12-19 19:19:28 +0800

    init

---
+++ src/main/java/com/baizhi/entity/Menu.java
@@ -0,0 +1,24 @@
+package com.baizhi.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import tk.mybatis.mapper.annotation.KeySql;
+
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "menu")
+public class Menu implements Serializable {
+    @Id
+    @KeySql(useGeneratedKeys = true)
+    private Integer id;
+    private String title;
+    private String url;
+    private String iconcls;
+    private Integer parent_id;
+}
